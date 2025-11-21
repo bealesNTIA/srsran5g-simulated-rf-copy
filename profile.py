4a96c4d45b19eae1e117a785045ea434ea3349f3
Brad Eales <163561397+bealesNTIA@users.noreply.github.com>
2025-11-21 11:16:39 -0700

    Test

--- profile.py
+++ profile.py
@@ -54,7 +54,7 @@ pc.defineParameter(
     name="enable_vnc",
     description="Enable browser-based VNC server.",
     typ=portal.ParameterType.BOOLEAN,
-    defaultValue=True,
+    defaultValue=False,
     advanced=False,
 )
 
